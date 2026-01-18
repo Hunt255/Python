4cadafbfd9860e470e791c98ebdba9aa94967862
Hitops <kuharchukivan123@gmail.com>
2026-01-18 16:24:39 +0200

    Added home work 7,3

--- Lesson7.py
+++ Lesson7.py
@@ -48,7 +48,27 @@
 # Input: Два рядки (String).
 # Output: Int or None
 
-
+# def second_index(text, some_str):
+#
+#     first_str = text.find(some_str)
+#
+#     if first_str == -1:
+#         return None
+#
+#     second_str = text.find(some_str, first_str + len(some_str))
+#
+#     if second_str == -1:
+#         return None
+#
+#     return second_str
+#
+#
+#
+# assert second_index("sims", "s") == 3, 'Test1'
+# assert second_index("find the river", "e") == 12, 'Test2'
+# assert second_index("hi", "h") is None, 'Test3'
+# assert second_index("Hello, hello", "lo") == 10, 'Test4'
+# print('ОК')
 
 # 7.4.Пошук спільних елементів
 # Напишіть функцію common_elements, яка згенерує два списки елементів з генераторного виразу
